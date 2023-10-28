b3e7a62c840cad25ee66cce46cb0d5c4e633bb8c
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-10-28 17:09:42 +0300

    test2

--- src/api.js
+++ src/api.js
@@ -3,18 +3,4 @@ import axios from 'axios';
 const apiKey = '40273804-3b8c2dbaae3f52338e7fd3d6d';
 const baseUrl = 'https://pixabay.com/api/';
 
-export function fetchImages(query, page, perPage) {
-  return axios
-    .get(baseUrl, {
-      params: {
-        key: apiKey,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: page,
-        per_page: perPage,
-      },
-    })
-    .then(response => response.data);
-}
+
